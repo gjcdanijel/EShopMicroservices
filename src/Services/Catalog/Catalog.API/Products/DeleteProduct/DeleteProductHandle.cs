c462d5755f875899642d2c872c01139a7c3cb16d
gjcdanijel <gajicdanijel5@gmail.com>
2025-06-21 19:08:46 +0200

    feat(Catalog.API): DeleteProduct and UpdateProduct Endpoints and Handlers

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandle.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id):ICommand<DeleteProductResult>;
+public record DeleteProductResult(bool IsSuccess);
+internal class DeleteProductCommandHandler
+	(IDocumentSession session, ILogger<DeleteProductCommand> logger)
+	:ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+	public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+	{
+		logger.LogInformation("DeleteProductCommandHandler. Handle called with {@Command}", command);
+
+		var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+		session.Delete<Product>(command.Id);
+		await session.SaveChangesAsync(cancellationToken);
+
+		return new DeleteProductResult(true);
+	}
+}
