a8aa52483edf4b0483769fe1d0f5afcf306040fc
Danijel <gajicdanijel5@gmail.com>
2026-04-21 15:07:25 +0200

    feature/Common.csproj

---
+++ src/Common/Common/CQRS/IQuery.cs
@@ -0,0 +1,9 @@
+using MediatR;
+
+namespace Common.CQRS;
+
+public interface IQuery<out TResponse> : IRequest<TResponse>
+where TResponse:notnull
+{
+    
+}
---
+++ src/Common/Common/CQRS/IQueryHandler.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics.CodeAnalysis;
+using MediatR;
+
+namespace Common.CQRS;
+
+public interface IQueryHandler<in TQuery, TResponse>
+    : IRequestHandler<TQuery, TResponse>
+        where TQuery : IQuery<TResponse>
+    where TResponse : notnull
+{
+    
+}
